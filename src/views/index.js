655fe1374789f429483d9238360e05eb52111137
Kevin Villalobos <dracojan_12@hotmail.com>
2021-07-19 00:36:45 -0500

    Use mixin in dashboard

--- src/views/index.js
+++ src/views/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 
 import Inbox from './Inbox';
 import Processes from './Processes';
+import dashboardViewMixin from './../mixins/DashboardViewMixin';
 
-Vue.component('app-inbox', Inbox);
+Vue.component(
+  'app-inbox',
+  Object.assign({}, Inbox, { mixins: [dashboardViewMixin] }),
+);
 Vue.component('app-processes', Processes);
