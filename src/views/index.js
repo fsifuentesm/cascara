0b8ab77034a9688b8c5f44d280160b35d6080017
Kevin Villalobos <dracojan_12@hotmail.com>
2021-07-26 11:59:03 -0500

    Delete mixin use

--- src/views/index.js
+++ src/views/index.js
@@ -2,10 +2,6 @@ import Vue from 'vue';
 
 import Inbox from './Inbox';
 import Processes from './Processes';
-import dashboardViewMixin from './../mixins/DashboardViewMixin';
 
-Vue.component(
-  'app-inbox',
-  Object.assign({}, Inbox, { mixins: [dashboardViewMixin] }),
-);
+Vue.component('app-inbox', Inbox);
 Vue.component('app-processes', Processes);
