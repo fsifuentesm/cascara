c6833867e97aa9b0aadf06347fc5e6e7bdf6cba3
Francisco Sifuentes <fsifuentesm@hotmail.com>
2021-10-27 16:38:08 -0500

    :skull: add first store

---
+++ src/store/users.module.js
@@ -0,0 +1,51 @@
+import Vue from 'vue';
+import { userService } from '../services/user.service';
+
+const state = {
+  allItems: {},
+};
+
+const actions = {
+  getUserGroups({ commit }, identifier) {
+    userService.getUserGroups(identifier)
+      .then((groups) => {
+        commit('getUserGroupsSuccess', {
+          identifier,
+          groups,
+        });
+      });
+  },
+  getUser({ commit }, identifier) {
+    userService.getUser(identifier)
+      .then((user) => {
+        commit('getUserSuccess', {
+          identifier,
+          user,
+        });
+      });
+  },
+};
+
+const mutations = {
+  getUserSuccess(ste, payload) {
+    Vue.set(ste.allItems, payload.identifier, {
+      identifier: payload.user.identifier,
+      email: payload.user.email,
+      fullname: payload.user.fullname,
+    });
+  },
+  getUserGroupsSuccess(ste, payload) {
+    Vue.set(ste.allItems, payload.identifier, {
+      groups: payload.groups,
+    });
+  },
+};
+
+export const users = {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+};
+
+export default users;
