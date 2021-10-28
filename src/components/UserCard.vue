c77fd9e96296a562a2072289878b04a6212af96a
Francisco Sifuentes <fsifuentesm@hotmail.com>
2021-10-28 10:55:54 -0500

    :skull: add component files

---
+++ src/components/UserCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card no-body">
+    <div class="container-fluid">
+      <small class="text-muted">
+        {{ user.identifier }}
+      </small><br>
+      <b>{{ user.fullname }}</b><br>
+      <small>
+        {{ user.email }}
+      </small>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
