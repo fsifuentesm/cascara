256ec7ef8447702c2ffe32b919a1f7e162ac6638
Francisco Sifuentes <fsifuentesm@hotmail.com>
2021-11-03 10:56:44 -0600

    :skull: register store pointer and execution

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import { users } from './users.module';
+import { executions } from './executions.module';
 import { groups } from './groups.module';
+import { pointers } from './pointers.module';
+import { users } from './users.module';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   modules: {
-    users,
+    executions,
     groups,
+    pointers,
+    users,
   },
 });
 
