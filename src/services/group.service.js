e1e97c2db9a958cefed34eb19cbae8c43f2c1895
Francisco Sifuentes <fsifuentesm@hotmail.com>
2021-11-03 09:41:04 -0600

    :skull: add service group

---
+++ src/services/group.service.js
@@ -0,0 +1,55 @@
+import ApiService from './api.service';
+import { getAuthToken } from '../utils/auth';
+
+// TODO: dry
+function authHeader() {
+  const cAuth = getAuthToken();
+  let auth = null;
+
+  if (typeof window !== 'undefined') {
+    auth = btoa(cAuth);
+  } else {
+    auth = new Buffer(cAuth).toString('base64');
+  }
+
+  if (auth) {
+    return { Authorization: `Basic ${auth}` };
+  }
+
+  return { };
+}
+
+function getGroups() {
+  return ApiService().get(
+    '/v1/group',
+    Object.assign(
+      { headers: authHeader() },
+    ),
+  )
+    .then(({ data }) => data.items)
+    .catch(error => Promise.reject(error));
+}
+
+function getGroup(identifier) {
+  const urlId = encodeURIComponent(identifier);
+
+  return ApiService().get(
+    `/v1/group/${urlId}`,
+    Object.assign(
+      { headers: authHeader() },
+    ),
+  )
+    .then(({ data }) => data)
+    .catch(error => Promise.reject(error));
+}
+
+export const groupService = {
+  getGroups,
+  getGroup,
+};
+
+export default {
+  install(Vue, name = '$groupService') {
+    Object.defineProperty(Vue.prototype, name, { value: groupService });
+  },
+};
