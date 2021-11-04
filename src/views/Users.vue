1df20c7cf9abda9698662256f0be9d5f22014bff
Francisco Sifuentes <fsifuentesm@hotmail.com>
2021-11-04 10:55:59 -0600

    :skull: add Users view

---
+++ src/views/Users.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container-fluid p-0" style="max-width: 1270px;">
+    <div class="row">
+      <div class="col">
+        <div v-for="user in currentUsers"
+          :key="user.id">
+          <user-list v-bind:user="user.data">
+          </user-list>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState({
+      users: state => state.users.allItems,
+    }),
+
+    currentUsers() {
+      if (this.users) {
+        return this.users;
+      }
+      return null;
+    },
+
+  },
+
+  methods: {
+    ...mapActions('users', {
+      getAllUsers: 'getAllUsers',
+    }),
+  },
+
+  props: {
+  },
+
+  mounted() {
+    this.getAllUsers();
+  },
+
+  watch: {
+    identifier: {
+      handler() {
+        this.getAllUsers();
+      },
+    },
+  },
+
+};
+</script>
+
+<style type="text/css"></style>
